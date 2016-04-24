3ce367466513148758b61b87d325b3bff4b8f28f
IgorShadurin <haletskaya@gmail.com>
2016-04-25 01:20:56 +0300

    Update pdaemon.sh

--- pdaemon.sh
+++ pdaemon.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-#nohup -q yii lead-maker/parse-groups 1 > parse_groups1.log 2>&1 &
+#nohup php -q yii lead-maker/parse-groups 3 > parse_groups3.log 2>&1 &
 
-nohup -q $1 $2 1 > /var/www/logs/nohup/$3 2>&1 &
+nohup php -q yii lead-maker/parse-groups 3 > parse_groups3.log 2>&1 &
+nohup php -q yii lead-maker/parse-groups 2 > parse_groups2.log 2>&1 &
+nohup php -q yii lead-maker/parse-groups 1 > parse_groups1.log 2>&1 &
